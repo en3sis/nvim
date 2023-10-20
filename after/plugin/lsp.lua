8714d482751111d65564fe6322611a3ed0e0f2b0
en3sis <940170+en3sis@users.noreply.github.com>
2023-10-20 10:09:50 +0200

    small fixes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,24 +1,27 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 end)
+
+lsp_zero.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['tsserver'] = {'javascript', 'typescript'},
+    ['rust_analyzer'] = {'rust'},
+  }
+})
+
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {"eslint",  "lua_ls", "rust_analyzer" },
-    handlers = {
-        lsp_zero.default_setup,
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
   },
 })
-require('lspconfig').lua_ls.setup({})
-require'lspconfig'.rust_analyzer.setup{
-  settings = {
-    ['rust-analyzer'] = {
-      diagnostics = {
-        enable = false;
-      }
-    }
-  }
-}

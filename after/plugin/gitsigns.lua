45de53d981fdef6485a33f04c4f35a1a6baf2b68
en3sis <940170+en3sis@users.noreply.github.com>
2023-10-25 16:42:25 +0200

    keybinds

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -21,13 +21,13 @@ require('gitsigns').setup {
       if vim.wo.diff then return ']c' end
       vim.schedule(function() gs.next_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, { expr = true })
 
     map('n', '[c', function()
       if vim.wo.diff then return '[c' end
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
-    end, {expr=true})
+    end, { expr = true })
 
     -- Actions
     --map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
@@ -39,12 +39,13 @@ require('gitsigns').setup {
     --map('n', '<leader>hp', gs.preview_hunk)
     --map('n', '<leader>hb', function() gs.blame_line{full=true} end)
     map('n', '<leader>gb', gs.toggle_current_line_blame)
+    map('n', '<leader>gd', gs.diffthis)
+
     --map('n', '<leader>hd', gs.diffthis)
     --map('n', '<leader>hD', function() gs.diffthis('~') end)
     --map('n', '<leader>td', gs.toggle_deleted)
 
     -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
   end
 }
-

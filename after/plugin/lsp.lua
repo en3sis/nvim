4563d64bad3169e877d1549e9f556e63dd240df9
en3sis <940170+en3sis@users.noreply.github.com>
2024-03-02 13:01:15 +0100

    small fixes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,7 +16,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { 'tsserver', 'eslint', 'rust_analyzer' },
+  ensure_installed = { 'rust_analyzer' },
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()

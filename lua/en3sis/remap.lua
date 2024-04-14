cb64f781d79b322c9f3cbf516be07337c00f7041
en3sis <940170+en3sis@users.noreply.github.com>
2024-04-14 12:55:47 +0200

    cleans rempas

--- lua/en3sis/remap.lua
+++ lua/en3sis/remap.lua
@@ -32,7 +32,7 @@ end)
 vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")
 
-
+-- VIM Inspector
 vim.keymap.set('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>")
 vim.keymap.set('n', "Dw", ":call vimspector#AddWatch()<cr>")
 vim.keymap.set('n', "De", ":call vimspector#Evaluate()<cr>")
@@ -42,16 +42,6 @@ vim.keymap.set('n', "De", ":call vimspector#Evaluate()<cr>")
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- LSP & Diagnostics
-vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
-vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-vim.keymap.set("n", "<F3>", function() vim.lsp.buf.code_action() end, opts)
-vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-vim.keymap.set("n", "<F2>", function() vim.lsp.buf.rename() end, opts)
-vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
   {silent = true, noremap = true}
 )
